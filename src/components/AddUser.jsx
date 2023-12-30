c77d883d1e5b27aa7fe23af0ff09ced25f90ffe9
IshaKumari10 <isha02740@gmail.com>
2023-12-30 12:56:42 +0530

    1:02:53

--- src/components/AddUser.jsx
+++ src/components/AddUser.jsx
@@ -1,27 +1,54 @@
-import {FormControl, FormGroup, InputLabel,Input} from '@mui/material';
+
+import {useState} from 'react';
+import {FormControl, FormGroup, InputLabel,Input,Typography,styled,Button} from '@mui/material';
+const Container=styled(FormGroup)`
+      width:50%;
+      margin:5% auto 0 auto;
+      & > div{
+        margin-top:20px;
+      }
+`
+const defaultValue={
+    name:'',
+    username:'',
+    email:'',
+    phone:''
+}
 
 
 const AddUser=()=>{
+    const[user,setUser]=useState(defaultValue);
+
+    const onValueChange=(e)=>{
+        setUser({ ...user,[e.target.name]:e.target.value});
+    }
+    const addUserDetails=()=>{
+
+    }
     return (
-        <FormGroup>
+        <Container>
+            <Typography variant="h4">Add user</Typography>
             <FormControl>
                 <InputLabel>Name</InputLabel>
-                <Input/>
+                <Input onChange={(e)=>onValueChange(e)} name="name"/>
             </FormControl>
             <FormControl>
                 <InputLabel>Username</InputLabel>
-                <Input/>
+                <Input onChange={(e)=>onValueChange(e)} name="username"/>
             </FormControl>
             <FormControl>
                 <InputLabel>Email</InputLabel>
-                <Input/>
+                <Input onChange={(e)=>onValueChange(e)}name="email"/>
             </FormControl>
             <FormControl>
                 <InputLabel>Phone</InputLabel>
-                <Input/>
+                <Input onChange={(e)=>onValueChange(e)}name="phone"/>
+            </FormControl>
+            <FormControl>
+                <Button variant="contained" onClick={()=>addUserDetails()}>Add User</Button>
             </FormControl>
            
-        </FormGroup>
+        </Container>
     )
 }
 export default AddUser;
