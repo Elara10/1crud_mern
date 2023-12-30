580c2a30eb2e48ece31e289e2f907ef505748393
IshaKumari10 <isha02740@gmail.com>
2023-12-30 11:57:03 +0530

    Name Username email Phone

--- src/components/AddUser.jsx
+++ src/components/AddUser.jsx
@@ -1,10 +1,26 @@
-import {FormGroup} from '@mui/material';
+import {FormControl, FormGroup, InputLabel,Input} from '@mui/material';
 
 
 const AddUser=()=>{
     return (
         <FormGroup>
-            
+            <FormControl>
+                <InputLabel>Name</InputLabel>
+                <Input/>
+            </FormControl>
+            <FormControl>
+                <InputLabel>Username</InputLabel>
+                <Input/>
+            </FormControl>
+            <FormControl>
+                <InputLabel>Email</InputLabel>
+                <Input/>
+            </FormControl>
+            <FormControl>
+                <InputLabel>Phone</InputLabel>
+                <Input/>
+            </FormControl>
+           
         </FormGroup>
     )
 }
